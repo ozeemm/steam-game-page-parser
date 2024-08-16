77951adcb1adc3fce8acccb7334a07e456ed0370
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-08-17 03:38:35 +0800

    Added Swagger

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseDefaultFiles(); // Отправка статических веб-страниц по умолчанию без обращения к ним по полному пути
 app.UseStaticFiles(); // обрабатывает все запросы к wwwroot
+
+app.MapGet("/test", () => 
+{
+    
+});
 
 app.Run();
