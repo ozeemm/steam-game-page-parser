93b332c2488ce6250e7c7addac1b4c6b9a7c49a0
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-08-17 09:16:29 +0800

    Added bootstrap

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -2,18 +2,20 @@ function showPage() {
     gameName = document.getElementById("GameName").value;
     statusLogger = document.getElementById("StatusLog");
     
+    statusLogger.innerHTML = "Загрузка...";
+
     const url = "/GetGamePage?name=" + gameName;
 
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.send();
+    xhr.open("POST", url, true);
     xhr.onload = () => {
-        document.body.innerHTML += xhr.response;
+        document.documentElement.innerHTML += '<html>' + xhr.response + '</html>';
     }
     xhr.onprogress = (event) => {
         statusLogger.innerHTML = 'Получено с сервера ' + event.loaded + ' байт из ' + event.total;
     }
     xhr.onerror = () => {
-        statusLogger.innerHTML += "Error!";
+        statusLogger.innerHTML = "Error!";
     }
+    xhr.send();
 }
